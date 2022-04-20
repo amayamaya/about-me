6820c9283873c45d5fb852b34f29ee62ee6d984f
‘AMAYA <aamaya547@gmail.com>
2022-04-20 15:43:13 -0700

    console shows button click

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 // import functions
 
 // grab DOM elements
-
+const showButton = document.getElementById('animal-button');
+const animal = document.getElementById('animal-div');
+    // console.log(showButton, animal); works!
 // set event listeners 
+showButton.addEventListener('click', () => {
+    console.log('show button clicked');
+    
+});
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
