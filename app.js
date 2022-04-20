18fadc00f71967bcc8ece5e3d9ed80ed03d909ba
‘AMAYA <aamaya547@gmail.com>
2022-04-20 16:47:30 -0700

    button working - added bio pic and copy

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const animal = document.getElementById('animal-div');
 // set event listeners 
 showButton.addEventListener('click', () => {
     //console.log('show button clicked');
-    
+    //remove hidden class and display animal
+    animal.classList.remove('hidden');
 });
+
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
